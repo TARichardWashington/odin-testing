2bac7a6134bf7c4dc1c3fa2f65c9d636f48dbd9a
Richard Washington <richard.washington@taxassist.co.uk>
2023-02-08 21:32:41 +0000

    :sparkles: Add an exported module class with basic calculator functions

--- calculator.test.js
+++ calculator.test.js
@@ -1,17 +1,19 @@
-import calculator from './calculator';
+import Calculator from './calculator';
+
+let calc = new Calculator();
 
 test('Calculator can add two numbers', () => {
-    expect(calculator.add(2, 3)).toBe(5);
+    expect(calc.add(2, 3)).toBe(5);
 });
 
 test('Calculator can multiply two numbers', () => {
-    expect(calculator.add(2, 3)).toBe(6);
+    expect(calc.multiply(2, 3)).toBe(6);
 });
 
 test('Calculator can subtract one number from another', () => {
-    expect(calculator.add(3, 2)).toBe(1);
+    expect(calc.subtract(3, 2)).toBe(1);
 });
 
 test('Calculator can divide one number by another', () => {
-    expect(calculator.add(10, 5)).toBe(2);
+    expect(calc.divide(10, 5)).toBe(2);
 });
