bfc4c6b358cc67385eeec4b7dd480039a02df76e
Richard Washington <richard.washington@taxassist.co.uk>
2023-02-10 16:29:00 +0000

    :test_tube:  Add test to ensure z correctly wraps to a

--- ceaser.test.js
+++ ceaser.test.js
@@ -3,3 +3,7 @@ import ceaser from './ceaser';
 test('Ceaser correctly enciphers strings', () => {
     expect(ceaser('banana')).toBe('cbobob');
 });
+
+test('Ceaser correctly ciphers z to a', () => {
+    expect(ceaser('zebedy')).toBe('acfez');
+});
