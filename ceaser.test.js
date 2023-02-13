664ad512b4ea5aab5bbf37afabcb7d09eaeb5a52
Richard Washington <richard.washington@taxassist.co.uk>
2023-02-13 23:04:30 +0000

    :test_tube: Fix test to ensure an accurate pass / fail

--- ceaser.test.js
+++ ceaser.test.js
@@ -5,5 +5,5 @@ test('Ceaser correctly enciphers strings', () => {
 });
 
 test('Ceaser correctly ciphers z to a', () => {
-    expect(ceaser('zebedy')).toBe('acbez');
+    expect(ceaser('zebedy')).toBe('afcfez');
 });
