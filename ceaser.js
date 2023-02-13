77489852cdfe97e7ce13cc45703eb8f8b46c5303
Richard Washington <richard.washington@taxassist.co.uk>
2023-02-13 22:44:56 +0000

    :bug: Ensure z correctly wraps to a

--- ceaser.js
+++ ceaser.js
@@ -3,6 +3,9 @@ export default function ceaser(string) {
     let splitString = string.split('');
 
     splitString = splitString.map((char) => {
+        if (char === 'z') {
+            return 'a';
+        }
         return String.fromCharCode(char.charCodeAt(0) + 1);
     });
 
