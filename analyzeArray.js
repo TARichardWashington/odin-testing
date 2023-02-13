a056547398c3f8d2ac21e1edd25b32b2b6d7ce74
Richard Washington <richard.washington@taxassist.co.uk>
2023-02-13 23:49:53 +0000

    :sparkles:  Throw an error if any element in the array passed for alaysis is not an integer

--- analyzeArray.js
+++ analyzeArray.js
@@ -4,6 +4,12 @@ export default function analyzeArray(arrayToBeAnalyzed) {
         throw new TypeError('Array required to be analyzed but an ' + typeof (arrayToBeAnalyzed) + ' passed');
     }
 
+    arrayToBeAnalyzed.forEach(element => {
+        if (!Number.isInteger(element)) {
+            throw new TypeError('All array elements must be integers but an ' + typeof (element) + 'was given');
+        }
+    });
+
     let average = (arrayToBeAnalyzed.reduce((accumulator, current) => {
         return accumulator + current;
     }, 0)) / arrayToBeAnalyzed.length;
