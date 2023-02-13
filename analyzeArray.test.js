9e4b5b33c8330a55afb4371f6ce8af0eff23ec02
Richard Washington <richard.washington@taxassist.co.uk>
2023-02-13 23:24:01 +0000

    :test_tube: Add test to ensure only arrays can be given to the array analyzer and if not a type error should be thrown

--- analyzeArray.test.js
+++ analyzeArray.test.js
@@ -6,3 +6,7 @@ test('Array returns correct average', () => {
     expect(analyzedArray.average).toBe(4);
 });
 
+test('Only an array can be passed to analyzeArray', () => {
+    expect(analyzeArray('hello').toThrow(TypeError));
+});
+
